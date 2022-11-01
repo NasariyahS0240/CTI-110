a317ce9829894d52c8880fddb9eaf7e71e088a46
NasariyahS0240 <117188442+NasariyahS0240@users.noreply.github.com>
2022-11-01 11:49:48 -0400

    PHPW1

---
+++ repo.py
@@ -0,0 +1,16 @@
+# CTI 110
+# P1HW1
+# Simple Input Output
+
+# Ask the user for a number
+print("please enter a number: ")
+num = int( input() ) 
+
+print("your number is", num)
+
+print ("that number squared is", num * num)
+
+# print num + 5
+print (num + 5)
+# print num * 5
+print (num * 5)
